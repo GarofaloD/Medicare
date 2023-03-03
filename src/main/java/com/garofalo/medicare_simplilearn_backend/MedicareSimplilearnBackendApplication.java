dbb6fce012d5c635f9e1e7793d15b0c388389bed
danielgarofalo <garofalodaniel@gmail.com>
2023-03-02 19:13:52 -0600

    application properties added

--- src/main/java/com/garofalo/medicare_simplilearn_backend/MedicareSimplilearnBackendApplication.java
+++ src/main/java/com/garofalo/medicare_simplilearn_backend/MedicareSimplilearnBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MedicareSimplilearnBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MedicareSimplilearnBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MedicareSimplilearnBackendApplication.class, args);
+    }
 
 }
