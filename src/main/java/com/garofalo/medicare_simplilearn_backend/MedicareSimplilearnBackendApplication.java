590a8f2072990583242561d1a142a4a6b116f3a9
danielgarofalo <garofalodaniel@gmail.com>
2023-03-02 19:09:36 -0600

    first commit

---
+++ src/main/java/com/garofalo/medicare_simplilearn_backend/MedicareSimplilearnBackendApplication.java
@@ -0,0 +1,13 @@
+package com.garofalo.medicare_simplilearn_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicareSimplilearnBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicareSimplilearnBackendApplication.class, args);
+	}
+
+}
