453a13ff5baeb69ab02067634c7069624fd43d01
Daniel Garofalo <garofalodaniel@gmail.com>
2023-03-04 20:57:33 -0600

    Cross origin added

--- src/main/java/com/garofalo/medicare_simplilearn_backend/repository/ProductManufacturerRepository.java
+++ src/main/java/com/garofalo/medicare_simplilearn_backend/repository/ProductManufacturerRepository.java
@@ -3,7 +3,9 @@ package com.garofalo.medicare_simplilearn_backend.repository;
 import com.garofalo.medicare_simplilearn_backend.entity.ProductManufacturer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productManufacturer", path="product-manufacturer")
 public interface ProductManufacturerRepository extends JpaRepository<ProductManufacturer, Integer> {
 }
