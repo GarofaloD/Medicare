2e815f2b6622e80f9eb08fca598d3d80480fa2e2
Daniel Garofalo <garofalodaniel@gmail.com>
2023-03-07 22:13:39 -0600

    serializing entity id

--- src/main/java/com/garofalo/medicare_simplilearn_backend/configuration/DataRestConfiguration.java
+++ src/main/java/com/garofalo/medicare_simplilearn_backend/configuration/DataRestConfiguration.java
@@ -3,16 +3,35 @@ package com.garofalo.medicare_simplilearn_backend.configuration;
 import com.garofalo.medicare_simplilearn_backend.entity.Product;
 import com.garofalo.medicare_simplilearn_backend.entity.ProductCategory;
 import com.garofalo.medicare_simplilearn_backend.entity.ProductManufacturer;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
+import jakarta.persistence.metamodel.Type;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class DataRestConfiguration implements RepositoryRestConfigurer {
 
+
+    //Properties
+    private EntityManager entityManager;
+
+    //Constructor
+
+
+    public DataRestConfiguration(EntityManager theEntityManager) {
+      entityManager = theEntityManager;
+    }
+
+    //Custom Methods
     @Override //Necessary method
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
-        //RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+       RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
 
         //unsopported actions (temporarily)
         //HttpMethod[] unsopportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
@@ -35,5 +54,45 @@ public class DataRestConfiguration implements RepositoryRestConfigurer {
                 .forDomainType(ProductManufacturer.class);
 
 
+
+        //Call internal helper method to expose id of categories
+        //exposeIds(config);
+
+//        config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream()
+//                .map(e -> e.getJavaType()).collect(Collectors.toList()).toArray(new Class[0]));
+
+
+        Class[] classes = entityManager.getMetamodel()
+                .getEntities().stream().map(Type::getJavaType).toArray(Class[]::new);
+        config.exposeIdsFor(classes);
     }
+
+
+
+
+
+//    private void exposeIds(RepositoryRestConfiguration config) {
+//        //expose entity id
+//
+//        //get list of entity classes
+//        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+//
+//        //array of entities
+//        List<Class> entityClasses = new ArrayList<>();
+//
+//        //entity types
+//        for (EntityType tempEntityType: entities) {
+//            entityClasses.add(tempEntityType.getJavaType());
+//        }
+//
+//        //expose the entity ids
+//        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+//        config.exposeIdsFor(domainTypes);
+//
+//
+//
+//
+//    }
+
+
 }
