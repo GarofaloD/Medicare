5cd7886372150e411d05abdf4282161ad57b06d8
Daniel Garofalo <garofalodaniel@gmail.com>
2023-03-18 10:56:08 -0500

    Swagger dependecies added

--- src/main/java/com/garofalo/medicare_simplilearn_backend/MedicareSimplilearnBackendApplication.java
+++ src/main/java/com/garofalo/medicare_simplilearn_backend/MedicareSimplilearnBackendApplication.java
@@ -1,9 +1,11 @@
 package com.garofalo.medicare_simplilearn_backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class MedicareSimplilearnBackendApplication {
 
     public static void main(String[] args) {
